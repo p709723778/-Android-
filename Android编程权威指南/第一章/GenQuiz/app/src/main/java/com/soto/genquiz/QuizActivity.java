3499eb0bb47d10625c95cc5b546b4aa83aaa8cb4
Soto <p709723778@126.com>
2016-05-01 16:32:09 +0800

    @第一章 ok

--- Android编程权威指南/第一章/GenQuiz/app/src/main/java/com/soto/genquiz/QuizActivity.java
+++ Android编程权威指南/第一章/GenQuiz/app/src/main/java/com/soto/genquiz/QuizActivity.java
@@ -3,18 +3,41 @@ package com.soto.genquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrue_button;
+
+    private Button mFalse_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        mTrue_button = (Button) findViewById(R.id.true_button);
+        mTrue_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalse_button = (Button) findViewById(R.id.flase_button);
+        mFalse_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,R.string.false_button,Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
     @Override
-    public  boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_quiz, menu);
         return true;
     }
