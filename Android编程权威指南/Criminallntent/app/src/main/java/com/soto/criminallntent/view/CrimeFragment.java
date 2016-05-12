81dd3ff2d64c474a93275194cd3d2097e7a547b2
Soto <p709723778@126.com>
2016-05-13 01:12:36 +0800

    @第八章

--- Android编程权威指南/Criminallntent/app/src/main/java/com/soto/criminallntent/view/CrimeFragment.java
+++ Android编程权威指南/Criminallntent/app/src/main/java/com/soto/criminallntent/view/CrimeFragment.java
@@ -5,9 +5,13 @@ import android.support.annotation.Nullable;
 import android.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import com.soto.criminallntent.R;
@@ -18,6 +22,10 @@ public class CrimeFragment extends Fragment {
     private Crime mCrime;
 
     private EditText mTitleEditField;
+
+    private Button mDateButton;
+
+    private CheckBox mSolvedCheckBox;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,6 +39,18 @@ public class CrimeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+
+        mDateButton.setText(DateFormat.format("E,MM dd yyyy",mCrime.getDate()));
+        mDateButton.setEnabled(false);
+
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mCrime.setSolved(isChecked);
+            }
+        });
         mTitleEditField = (EditText) v.findViewById(R.id.crime_title);
         mTitleEditField.addTextChangedListener(new TextWatcher() {
             @Override
