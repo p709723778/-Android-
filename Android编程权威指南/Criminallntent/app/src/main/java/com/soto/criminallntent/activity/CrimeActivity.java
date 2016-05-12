7f7082144e75a152372ad238663ff0efc6a3bedd
Soto <p709723778@126.com>
2016-05-12 23:20:07 +0800

    no message

--- Android编程权威指南/Criminallntent/app/src/main/java/com/soto/criminallntent/activity/CrimeActivity.java
+++ Android编程权威指南/Criminallntent/app/src/main/java/com/soto/criminallntent/activity/CrimeActivity.java
@@ -4,11 +4,13 @@ import android.app.Fragment;
 import android.app.Activity;
 import android.os.Bundle;
 import android.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 import com.soto.criminallntent.R;
 import com.soto.criminallntent.view.CrimeFragment;
 
-public class CrimeActivity extends Activity {
+public class CrimeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +24,11 @@ public class CrimeActivity extends Activity {
             fragment = new CrimeFragment();
             fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_crime, menu);
+        return true;
     }
 }
